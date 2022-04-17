5ff835ac31bb8ed4a93e35e2d0cd710943c78ed9
Tamara Costa <tamarasantosc@gmail.com>
2022-04-17 14:20:48 -0300

    to-do-list

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
-
+  public taskList : Array<TaskList> = []
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  teste(){
+    alert(5)
   }
 
 }
