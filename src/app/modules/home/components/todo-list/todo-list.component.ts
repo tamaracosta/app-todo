843079846d4f93b3c88c46bd4065d4186c176fd9
Tamara Costa <tamarasantosc@gmail.com>
2022-05-03 14:03:57 -0300

    usando localStorage para salvar os dados

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { TaskList } from '../../model/task-list';
 
 @Component({
@@ -6,15 +6,13 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
-  public taskList : Array<TaskList> = [
-    {task: "Minha nova task", checked: true},
-    {task: "Minha nova task2", checked: false}
-
-  ]
+export class TodoListComponent implements DoCheck {
+  public taskList : Array<TaskList> = JSON.parse(localStorage.getItem("list") || '[]');
   constructor() { }
 
-  ngOnInit(): void {
+  ngDoCheck(): void {
+    this.setLocalStorage()
+
   }
 
   deleteItemTaskList(event: any){
@@ -31,4 +29,22 @@ export class TodoListComponent implements OnInit {
   setEmitItemTaskList(event: string){
     this.taskList.push({task: event, checked: false})
   }
+
+  validationInput(event: string, index: number){
+    if(!event.length){
+      const confirm = window.confirm("Tarefa vazia! Deseja deletar?")
+      if(confirm){
+        this.deleteItemTaskList(index)
+      }
+    }
+  }
+
+  setLocalStorage(){
+    if(this.taskList){
+      this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked))
+      localStorage.setItem("list", JSON.stringify(this.taskList))
+
+    }
+  }
+
 }
